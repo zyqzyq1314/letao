4c393a3654488463d1bc163b0f9a964cb4d1762d
zyq <1427698389@qq.com>
2018-05-10 11:59:06 +0800

    公共页面所有功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -2,29 +2,67 @@
  * Created by lenovo on 2018/5/9.
  */
 
-// 希望效果是, 每次ajax提交, 产生进度条, ajax完成, 结束进度条
-/*
- * ajax 全局事件
- *   ajaxComplete 只要请求完成就调用 (不管成功或者失败)
- *   ajaxError 请求失败时调用
- *   ajaxSuccess 请求成功时调用
- *
- *   ajaxSend  请求发送时调用
- *
- *   ajaxStart  第一个ajax开始发送的时候调用
- *   ajaxStop   最后一个ajax结束时调用
- * */
-// 禁用进度环
-NProgress.configure({ showSpinner: false });
 
-$(document).ajaxStart(function () {
-  //开启进度条
-  NProgress.start();
+//进度条功能
+$(function () {
+
+// 希望效果是, 每次ajax提交, 产生进度条, ajax完成, 结束进度条
+  /*
+   * ajax 全局事件
+   *   ajaxComplete 只要请求完成就调用 (不管成功或者失败)
+   *   ajaxError 请求失败时调用
+   *   ajaxSuccess 请求成功时调用
+   *
+   *   ajaxSend  请求发送时调用
+   *
+   *   ajaxStart  第一个ajax开始发送的时候调用
+   *   ajaxStop   最后一个ajax结束时调用
+   * */
+// 禁用进度环
+  NProgress.configure({ showSpinner: false });
+  
+  $(document).ajaxStart(function () {
+    //开启进度条
+    NProgress.start();
+  })
+  $(document).ajaxStop(function() {
+    // 模拟网络环境
+    setTimeout(function() {
+      // 结束进度条
+      NProgress.done();
+    }, 1000);
+  });
 })
-$(document).ajaxStop(function() {
-  // 模拟网络环境
-  setTimeout(function() {
-    // 结束进度条
-    NProgress.done();
-  }, 1000);
-});
+
+
+$(function () {
+  // 1. 二级分类切换功能
+  $('.category').click(function() {
+    $(this).next().stop().slideToggle();
+  });
+  //2. 菜单栏点击消失,出现
+    $('.icon-menu').on('click',function () {
+        $('.lt_aside').toggleClass('toggle');
+        $('.lt_topbar').toggleClass('toggle');
+        $('.lt_main').toggleClass('toggle');
+    })
+  //点击推出按钮,显示推出模态框
+  $('.icon-logout').on('click',function () {
+    $('#logoutModal').modal('show');
+  })
+  //当点击推出按钮时,发送ajax请求,退出出登录
+   $('#logoutBtn').on('click',function () {
+     console.log(1111);
+      $.ajax({
+        type:'get',
+        url:'/employee/employeeLogout',
+         dataType:'json',
+          success:function (info) {
+            // console.log(info);
+            if(info.success){
+              location.href="login.html";
+            }
+          }
+      })
+   })
+})
